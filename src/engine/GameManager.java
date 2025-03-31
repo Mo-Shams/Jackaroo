08ad6bbaf0ad7b46124121f39f5c47026b930a35
Youssef Tarek <yt51705@gmail.com>
2025-03-31 09:41:33 +0200

    Added interface methods

--- src/engine/GameManager.java
+++ src/engine/GameManager.java
@@ -1,5 +1,14 @@
 package engine;
 
-public interface GameManager {
+import exception.*;
+import model.*;
+import model.player.Marble;
 
+public interface GameManager {
+	void sendHome(Marble marble);
+    void fieldMarble() throws CannotFieldException, IllegalDestroyException;
+    void discardCard(Colour colour) throws CannotDiscardException;
+    void discardCard() throws CannotDiscardException;
+    Colour getActivePlayerColour();
+    Colour getNextPlayerColour();
 }
--- src/engine/board/BoardManager.java
+++ src/engine/board/BoardManager.java
@@ -1,5 +1,16 @@
 package engine.board;
 
+import java.util.ArrayList;
+
+import exception.*;
+import model.player.Marble;
+
 public interface BoardManager {
 	int getSplitDistance();
+	void moveBy(Marble marble, int steps, boolean destroy) throws IllegalMovementException, IllegalDestroyException;
+	void swap(Marble marble1, Marble marble2) throws IllegalSwapException;
+    void destroyMarble(Marble marble) throws IllegalDestroyException;
+    void sendToBase(Marble marble) throws CannotFieldException, IllegalDestroyException;
+    void sendToSafe(Marble marble) throws InvalidMarbleException;
+    ArrayList<Marble> getActionableMarbles();
 }
