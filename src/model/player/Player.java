4e1cc821264db981640d8d315b7c25aa2fbd6ac8
Muhammad Walid <mohamedwaleid811@gmail.com>
2025-04-03 09:40:45 +0200

    finalized the whole player package

--- src/model/player/Player.java
+++ src/model/player/Player.java
@@ -9,7 +9,7 @@ import model.Colour;
 import model.card.Card;
 
 public class Player {
-
+	//instance variables
 	private final String name;
 	private final Colour colour;
 	private ArrayList<Card> hand;
@@ -17,6 +17,8 @@ public class Player {
 	private Card selectedCard;
 	private final ArrayList<Marble> selectedMarbles;
 	
+	
+	//constructor
 	public  Player(String name, Colour colour){
 		this.name = name;
 		this.colour = colour;
@@ -26,30 +28,102 @@ public class Player {
 		for(int i=0; i<4; i++) this.marbles.add(new Marble(colour));
 		this.selectedCard = null;
 	}
-
+	
+	
+	
+	
+	
+	
+	
+	//simple getter and setter methods for the instance variables
 	public ArrayList<Card> getHand() {
 		return hand;
 	}
-
 	public void setHand(ArrayList<Card> hand) {
 		this.hand = hand;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public Colour getColour() {
 		return colour;
 	}
-
 	public ArrayList<Marble> getMarbles() {
 		return marbles;
 	}
-
 	public Card getSelectedCard() {
 		return selectedCard;
 	}
+
 	
+	
+	
+	
+	
+	//method to add the specified marble to the player's home zone
+	public void regainMarble(Marble marble){
+		marbles.add(marble);
+	}
+	
+	
+	
+	
+	
+	
+	//getter method for the marbles in the home zone if any
+	public Marble getOneMarble(){
+		if(marbles.isEmpty())
+			return null;
+		return marbles.get(0);
+	}
+	
+	
+	
+	
+	
+	
+	
+	//methods that validates and selects the given card and marble
+	public void selectCard(Card card) throws InvalidCardException {
+		boolean exist = false;
+		for(Card currentCard : hand){
+			if(currentCard.equals(card)){
+				selectedCard = currentCard;
+				exist = true;
+			}
+		}
+		if(!exist) throw new InvalidCardException("You don't have this card");
+	}
+	public void selectMarble(Marble marble) throws InvalidMarbleException{
+		if(selectedMarbles.size() == 2)
+			throw new InvalidMarbleException("You can't choose more than 2 marbles");
+		selectedMarbles.add(marble);
+	}
+	
+	
+	
+	
+	
+	
+	
+	//method that resets the player choices, both card and marble
+	public void deselectAll(){
+		selectedCard = null;
+		selectedMarbles.clear();
+	}
+	
+	
+	
+	
+	
+	
+	
+	//method that executes the selected card action on the selected marbles if valid
+	public void play() throws GameException{
+		if(selectedCard == null) throw new InvalidCardException("You didn't choose a card yet");
+		if(!selectedCard.validateMarbleColours(selectedMarbles) || !selectedCard.validateMarbleSize(selectedMarbles))
+			throw new InvalidMarbleException("You cannot use the " + selectedCard.getName() + " card on the selected marble/s");
+		selectedCard.act(selectedMarbles);
+	}
 	
 }
